96b5d64407db3bdde6d7334fed80d2a12e3bf111
eruotolo <edgardoruotolo@outlook.com>
2023-03-02 11:11:27 -0300

    02-03-2023 - Update CSS

--- index-01.php
+++ index-01.php
@@ -424,7 +424,7 @@
                                     WHERE id_charla_doc = $idcharla";
                         $result_task1 = mysqli_query($conn, $query);
                         while ($row = mysqli_fetch_Array($result_task1)) {
-                            ?><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" fill="#707070">
+                            ?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" fill="#707070">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                             </svg>
                             <p><a href="<?php echo $row['ruta_doc'] ?>" download="<?php echo $row['nombre_doc'] ?>"><?php echo $row['nombre_doc'] ?></a></p>
